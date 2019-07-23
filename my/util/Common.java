bc1b25262c95440b8fe83d5d8f3bcc1ab63b8b11
yusuke <test@test>
2019-07-23 10:31:27 +0900

    降順ソートも追加

--- my/util/Common.java
+++ my/util/Common.java
@@ -28,4 +28,19 @@ public class Common{
 			}
 		}
 	}
+	public static void sort(int[] arr,boolean isDesc){
+		if(!isDesc){
+			sort(arr);	
+		}else{
+			for(int i=0;i<arr.length-1;i++){
+				for(int j=i+1;j<arr.length;j++){
+					if(arr[i]<arr[j]){
+						int temp=arr[i];
+						arr[i]=arr[j];
+						arr[j]=temp;
+					}
+				}
+			}
+		}
+	}
 }
