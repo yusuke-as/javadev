1980940c4d7b88ca87a05942d3b9f41fbfdc0cd5
yusuke <test@test>
2019-07-23 11:06:35 +0900

    bubblesortを追加

--- my/util/Common.java
+++ my/util/Common.java
@@ -43,4 +43,15 @@ public class Common{
 			}
 		}
 	}
+	public static void bubbleSort(int[] arr){
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=arr.length-1;j>i;j--){
+				if(arr[j]<arr[j-1]){
+					int temp=arr[j];
+					arr[j]=arr[j-1];
+					arr[j-1]=temp;
+				}
+			}
+		}
+	}
 }
