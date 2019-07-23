6facce397c7c0ce641a36cb024cf7ee2abde0dc8
yusuke <test@test>
2019-07-23 10:21:21 +0900

    Commonにshuffle,sortを追加

--- my/util/Common.java
+++ my/util/Common.java
@@ -1,10 +1,31 @@
 package my.util;
+import java.util.*;
 public class Common{
 	public static void reverse(int[] arr){
 		for(int i=0;i<arr.length/2;i++){
 			int temp=arr[i];
 			arr[i]=arr[arr.length-1-i];
 			arr[arr.length-1-i]=temp;
+		}
+	}
+	public static void shuffle(int[] arr){
+		Random rand=new Random();
+		for(int i=0;i<arr.length-1;i++){
+			int index=rand.nextInt(arr.length-i);
+			int temp=arr[index];
+			arr[index]=arr[arr.length-1-i];
+			arr[arr.length-1-i]=temp;
+		}
+	}
+	public static void sort(int[] arr){
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[i]>arr[j]){
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
 		}
 	}
 }
