b18cea3fe3f1cbb9a654956ce463cbd79f52a785
yusuke <test@test>
2019-07-23 09:35:17 +0900

    myフォルダの追加

---
+++ my/util/Arrays.java
@@ -0,0 +1,10 @@
+package my.util;
+public class Arrays{
+	public static void reverse(int[] arr){
+		for(int i=0;i<arr.length/2;i++){
+			int temp=arr[i];
+			arr[i]=arr[arr.length-1-i];
+			arr[arr.length-1-i]=temp;
+		}
+	}
+}
